9f479e6ba464d6803680c14011bc5816470c26d1
janickic <camille.janicki@gmail.com>
2018-04-03 16:14:03 -0700

    Adding quit button and in-code comments

--- project.py
+++ project.py
@@ -13,19 +13,19 @@ import matplotlib.pyplot as plt
 # Global Variables
 root = Tk()
 filename = ['']
-v = IntVar()
-v.set(0) #initially set to rows
-l_t = Label(root, text=None)
-l = Label(root, image=None)
-l2 = Label(root, image=None)
+# v = IntVar()
+# v.set(0) #initially set to rows
+label = Label(root, text=None)
+# l = Label(root, image=None)
+# l2 = Label(root, image=None)
 
 
 #-------------------------------------------------PART 1----------------------------------------------------#
 
 # PART 1 - STI by Copying Pixels (Copying Columns)
 def copyCol():
-	l_t.configure(text="Computing...")
-	l_t.pack()	
+	label.configure(text="Computing...")
+	label.pack()	
 	root.update()
 
 	cap = cv2.VideoCapture(filename[0])
@@ -36,11 +36,8 @@ def copyCol():
 	width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))   
 	height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
 	num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-	# print width #352
-	# print height #288
 
 	blank_image = np.zeros((height,num_frames,3), np.uint8)
-
 	count_frame=0
 	while(True):
 		# Capture frame-by-frame
@@ -48,9 +45,7 @@ def copyCol():
 		if ret == True:
 			col =  frame[:,width/2]
 			blank_image[:,count_frame] = col
-			# print len(frame) # 288
-			# print len(frame[0]) #352
-			# Our operations on the frame come here
+			# Operations on the frame 
 			color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 		    # Display the resulting frame
 			cv2.imshow('Collecting columns',color)
@@ -67,22 +62,20 @@ def copyCol():
 
 	img = Image.fromarray(blank_image, 'RGB')
 	img.save('ColCpyImg.png')
-	# img.show()
 	photo = cv2.imread('ColCpyImg.png',cv2.IMREAD_COLOR)
 	cv2.imshow('Hit space to exit',photo)
 	cv2.waitKey(0) & 0xFF
-	# cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
-	l_t.configure(text="Please choose again")
-	l_t.pack()
+	label.configure(text="Please choose again")
+	label.pack()
 	root.update()
 
 
 # PART 1 - STI by Copying Pixels (Copying Rows)
 def copyRow():
-	l_t.configure(text="Computing...")
-	l_t.pack()	
+	label.configure(text="Computing...")
+	label.pack()	
 	root.update()
 
 	cap = cv2.VideoCapture(filename[0])
@@ -118,15 +111,13 @@ def copyRow():
 	rot = np.rot90(blank_image)
 	img = Image.fromarray(rot, 'RGB')
 	img.save('RowCpyImg.png')
-	# img.show()
 	photo = cv2.imread('RowCpyImg.png',cv2.IMREAD_COLOR)
 	cv2.imshow('Hit space to exit',photo)
 	cv2.waitKey(0) & 0xFF
-	# cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
-	l_t.configure(text="Please choose again")
-	l_t.pack()
+	label.configure(text="Please choose again")
+	label.pack()
 	root.update()
 
 #-------------------------------------------------END OF PART 1----------------------------------------------------#
@@ -137,25 +128,30 @@ def copyRow():
 #----------------------------------------------------PART 2--------------------------------------------------------#
 
 # Change Chromaticity of frame
-def chromaticity(arr):
-	a = arr 
-	for j in range(len(a)):
-	    for i in range(len(a[0])):
-	        r, g, b = a[j][i][0], a[j][i][1], a[j][i][2]
-	        if(r==0)&(g==0)&(b==0):
-	            a[j][i][0], a[j][i][1], a[j][i][2] = 0, 0, 0
+def chromaticity(frame):
+	new_arr = frame 
+	for j in range(len(frame)):
+	    for i in range(len(frame[0])):
+	        R, G, B = frame[j][i][0], frame[j][i][1], frame[j][i][2]
+	        if(R==0)&(G==0)&(B==0):
+	            new_arr[j][i][0], new_arr[j][i][1], new_arr[j][i][2] = 0, 0, 0
 	        else:
-	            a[j][i][0] = int(round((((a[j][i][0]+0.0)/((r+0)+g+b))*255)))
-	            a[j][i][1] = int(round((((a[j][i][1]+0.0)/((r+0)+g+b))*255)))
-	            a[j][i][2] = int(round((((a[j][i][2]+0.0)/((r+0)+g+b))*255)))
-	return a
+	        	# r
+	            new_arr[j][i][0] = int(round((((R+0.0)/((R+0)+G+B))*255)))
+	            # g
+	            new_arr[j][i][1] = int(round((((G+0.0)/((R+0)+G+B))*255)))
+	            # b, won't use this
+	            new_arr[j][i][2] = int(round((((B+0.0)/((R+0)+G+B))*255)))
+	return new_arr
 
 # Make a histogram per frame
 def makeHist(img_arr):  
 	new_arr = [[],[]]
 	for i in range(len(img_arr)):
-		new_arr[0].append(img_arr[i][0]) #r
-		new_arr[1].append(img_arr[i][1]) #g
+		# r
+		new_arr[0].append(img_arr[i][0]) 
+		# g
+		new_arr[1].append(img_arr[i][1]) 
 
 	new_arr = np.asarray(new_arr, dtype=np.uint8)   
 	bins = int(1 + math.log(len(img_arr), 2)) 
@@ -176,8 +172,8 @@ def histList(img_arr):
 
 # PART 2 - STI by Histogram Differences (Copying Columns)
 def histCol():
-	l_t.configure(text="Please wait, computing may take a while")
-	l_t.pack()	
+	label.configure(text="Please wait, computing may take a while")
+	label.pack()	
 	root.update()
 	count = 0   
 	cap = cv2.VideoCapture(filename[0]) 
@@ -196,7 +192,7 @@ def histCol():
         # Resize image for faster computation
 		frame = cv2.resize(frame, (32, 32))
 
-        # Array A stores chromaticity values
+        # Array A stores list of frames
 		if count == 0:
 			A = [chromaticity([frame[i]]) for i in range(32)]
 			count = count + 1
@@ -204,6 +200,7 @@ def histCol():
 			for i in range(32):
 				A[i].append(frame[i])
     
+    # Converting all frames from RGB to rg
 	for i in range(len(A)):
 		A[i] = chromaticity(A[i])
         
@@ -225,7 +222,6 @@ def histCol():
 	img = Image.fromarray(np.asarray(histogram_list, dtype=np.uint8), "L")
 
 	img.save('HistColImg.png')
-	# img.show()
 	photo = cv2.imread('HistColImg.png',cv2.IMREAD_COLOR)
 	
 	cv2.namedWindow('Hit space to exit',cv2.WINDOW_NORMAL)
@@ -235,14 +231,14 @@ def histCol():
 	cv2.waitKey(0) & 0xFF
 	cv2.destroyAllWindows()
 
-	l_t.configure(text="Please choose again")
-	l_t.pack()
+	label.configure(text="Please choose again")
+	label.pack()
 	root.update()
 
 # PART 2 - STI by Histogram Differences (Copying Rows)
 def histRow():
-	l_t.configure(text="Please wait, computing may take a while")
-	l_t.pack()	
+	label.configure(text="Please wait, computing may take a while")
+	label.pack()	
 	root.update()
 	count = 0   
 	cap = cv2.VideoCapture(filename[0]) 
@@ -260,7 +256,8 @@ def histRow():
         
         # Resize image for faster computation
 		frame = cv2.resize(frame, (32, 32))
-        # Array A stores chromaticity values
+
+        # Array A stores list of frames
 		if count == 0:
 			A = [chromaticity([frame[i]]) for i in range(32)]
 			count = count + 1
@@ -268,6 +265,7 @@ def histRow():
 			for i in range(32):
 				A[i].append(frame[i])
     
+    # Converting all frames from RGB to rg
 	for i in range(len(A)):
 		A[i] = chromaticity(A[i])
         
@@ -287,7 +285,6 @@ def histRow():
 	img = Image.fromarray(np.asarray(histogram_list, dtype=np.uint8), "L")
 
 	img.save('HistRowImg.png')
-	# img.show()
 	photo = cv2.imread('HistRowImg.png',cv2.IMREAD_COLOR)
 	
 	cv2.namedWindow('Hit space to exit',cv2.WINDOW_NORMAL)
@@ -297,8 +294,8 @@ def histRow():
 	cv2.waitKey(0) & 0xFF
 	cv2.destroyAllWindows()
 
-	l_t.configure(text="Please choose again")
-	l_t.pack()
+	label.configure(text="Please choose again")
+	label.pack()
 	root.update()
 
 #-------------------------------------------------END OF PART 2----------------------------------------------------#
@@ -332,6 +329,10 @@ def parseVideo(choice):
 		elif choice == 4:
 			histRow()
 
+# Close GUI
+def quit():
+	root.destroy()
+
 # Main Loop, Opens GUI 
 def main():
 	confGui()
@@ -341,6 +342,8 @@ def main():
 	Button(text="STI by Copying Pixels: Rows", width = 70, command=lambda : parseVideo(2)).pack()
 	Button(text="STI by Histogram Differences: Columns", width = 70, command=lambda : parseVideo(3)).pack()
 	Button(text="STI by Histogram Differences: Rows", width = 70, command=lambda : parseVideo(4)).pack()
+	Button(text="Exit", width = 50, command=lambda : quit()).pack()
+
 	root.mainloop() 
 
 main()
